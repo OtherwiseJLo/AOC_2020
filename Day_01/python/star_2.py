b67388fc09dba52c2e5a71ec8170e75b5eadbcdf
OtherwiseJLo <jonathanglopez@gmail.com>
2020-12-02 18:31:21 -0500

    Updated with python template

--- Day_01/python/star_2.py
+++ Day_01/python/star_2.py
@@ -1,10 +1,21 @@
+from aoc import aoc_read_input
+from os import path
+from typing import List
 from itertools import combinations
 
-with open("input", "r") as f:
-    expenses = [int(expense.strip()) for expense in f.readlines()]
 
-total = 2020
-for expens in combinations(expenses, 3):
-    if sum(expens) == total:
-        product = expens[0] * expens[1] * expens[2]
-        print(f"Product is {product}")
+def transform_input(input_vals: List) -> List:
+    return [int(val) for val in input_vals]
+
+
+def func1(input_vals: List):
+    total = 2020
+    for expens in combinations(input_vals, 3):
+        if sum(expens) == total:
+            return expens[0] * expens[1] * expens[2]
+
+
+if __name__ == "__main__":
+    input_vals = aoc_read_input(path.abspath(__file__))
+    input_vals = transform_input(input_vals)
+    print(func1(input_vals))
