a4f300c79d9d7603ef2e8470ac413953fc9676c4
OtherwiseJLo <jonathanglopez@gmail.com>
2020-12-02 20:48:51 -0500

    Fixed optimized bug

--- Day_01/python/star_2.py
+++ Day_01/python/star_2.py
@@ -15,7 +15,27 @@ def func1(input_vals: List):
             return expens[0] * expens[1] * expens[2]
 
 
+def get_complements(input_vals: List, total: int):
+    history = {}
+    for idx, val in enumerate(input_vals):
+        try:
+            result = history[val]
+            return result * val
+        except KeyError:
+            history[total - val] = val
+    return 0
+
+
+def func2(input_vals: List):
+    total = 2020
+    for idx, val in enumerate(input_vals):
+        complement = total - val
+        if (result := get_complements(input_vals[idx + 1 :], complement)) :
+            return val * result
+
+
 if __name__ == "__main__":
     input_vals = aoc_read_input(path.abspath(__file__))
     input_vals = transform_input(input_vals)
     print(func1(input_vals))
+    print(func2(input_vals))
